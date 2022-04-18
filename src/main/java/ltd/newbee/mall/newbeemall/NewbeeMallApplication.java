124b1ed7403342a30ee2a2e1aa9ab0e8b871868e
forEngineer-hub <70783654+forEngineer-hub@users.noreply.github.com>
2022-04-18 15:49:20 +0900

    add new goods contrller ~ dao

--- src/main/java/ltd/newbee/mall/newbeemall/NewbeeMallApplication.java
+++ src/main/java/ltd/newbee/mall/newbeemall/NewbeeMallApplication.java
@@ -1,8 +1,10 @@
 package ltd.newbee.mall.newbeemall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("ltd.newbee.mall.newbeemall.dao")
 @SpringBootApplication
 public class NewbeeMallApplication {
 
