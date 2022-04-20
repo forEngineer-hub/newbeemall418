303c0d6c84885eba3fe93eaa56ed8f7af2757703
forEngineer-hub <70783654+forEngineer-hub@users.noreply.github.com>
2022-04-20 21:19:08 +0900

    fix index config service

--- src/main/java/ltd/newbee/mall/newbeemall/service/NewBeeMallIndexConfigService.java
+++ src/main/java/ltd/newbee/mall/newbeemall/service/NewBeeMallIndexConfigService.java
@@ -8,5 +8,4 @@ import ltd.newbee.mall.newbeemall.vo.NewBeeMallIndexCategoryVO;
 public interface NewBeeMallIndexConfigService {
 	List<NewBeeMallGoodsDetailVO> getConfigGoodsesForIndex(int configType ,int number);
 	
-	List<NewBeeMallIndexCategoryVO> getCategoriesForIndex();
 }
