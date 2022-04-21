51a25780b80a4d097f2eb207bfa8dd4d24d67eb1
forEngineer-hub <70783654+forEngineer-hub@users.noreply.github.com>
2022-04-21 13:46:54 +0900

    add NewBeeMallCategoryServiceImpl

--- src/main/java/ltd/newbee/mall/newbeemall/service/NewBeeMallIndexConfigService.java
+++ src/main/java/ltd/newbee/mall/newbeemall/service/NewBeeMallIndexConfigService.java
@@ -7,4 +7,5 @@ import ltd.newbee.mall.newbeemall.vo.NewBeeMallIndexCategoryVO;
 
 public interface NewBeeMallIndexConfigService {
 	List<NewBeeMallGoodsDetailVO> getConfigGoodsesForIndex(int configType ,int number);
+
 }
